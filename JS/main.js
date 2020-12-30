da8490d4ca44f4f921038f10a2c0366259b00949
Kyr Roms <72196829+Lacrima98654@users.noreply.github.com>
2020-12-31 00:37:29 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,19 @@
-//Code JavaScript
+function randfunc(min ,max){//Fonction qui génère des nombres aléatoires
+    return Math.floor(Math.random()*(max+1))+min;
+}
+function viewportSize() {// Fonction qui donne les dimensions
+  var d = document.documentElement;
+  return {
+    height: d.clientHeight,
+    width: d.clientWidth
+  };
+}
+
+//Fonction principale
+$(function(){
+    $('body').append('<div class="fire" id="fire1"></div>');
+    $('#fire1').css({
+        'bottom':'150px',
+        'left':'500px'
+    });
+});
