0a0db771018568a189d43b207feebf065b37c09c
Kyr Roms <72196829+Lacrima98654@users.noreply.github.com>
2020-12-31 20:39:33 +0100

    Style

--- JS/main.js
+++ JS/main.js
@@ -9,15 +9,54 @@ function viewportSize() {// Fonction qui donne les dimensions
   };
 }
 
+function detonation(positionX,positionY,z){
+  let x,y;
+  for(let i = 1;i<= 5;i++){
+    $('body').append('<div class="explose" id="explose'+z+i+'"></div>');
+    $('#explose'+x+i).css({
+      'top':positionY+'px',
+      'left':positionX+'px',
+      'transition':0.5+'s'
+    });
+  }
+  x =positionX-20; y = positionY+20;
+  $('#explose'+z+1).css({
+    'top':y+'px',
+    'left':x+'px'
+  });
+  x =positionX-25; y = positionY-20;
+  $('#explose'+z+2).css({
+    'top':y,
+    'left':x
+  });
+  x =positionX; y = positionY-25;
+  $('#explose'+z+3).css({
+    'top':y,
+    'left':x
+  });
+  x =positionX+20; y = positionY-18;
+  $('#explose'+z+4).css({
+    'top':y,
+    'left':x
+  });
+  x =positionX+20; y = positionY+25;
+  $('#explose'+z+5).css({
+    'top':y,
+    'left':x
+  });
+}
+
 //Fonction principale
 $(function(){
-  let fire = 10, taille = viewportSize(), vitesse = [], positionY = [], positionX = [], moveX = [];
+  let fire = 10, taille = viewportSize(),duree_explosion = [], vitesse = [], positionY = [], positionX = [], stop = [];
   //Initialisation
   for(let i = 0; i <= fire;i++)
   {
-    vitesse[i] = 20;
+    vitesse[i] = 10;
     positionY[i] = randfunc(taille.height,taille.height+100);
     positionX[i] = randfunc(1,taille.width);
+    duree_explosion[i] = 0;
+    stop[i] = randfunc(20,120);
     $('body').append('<div class="fire" id="fire'+i+'"></div>');
     $('#fire'+i).css({
         'top':positionY[i]+'px',
@@ -28,9 +67,36 @@ $(function(){
     taille=viewportSize();
     for(let i = 0;i < fire; i++)
    {
-    positionY[i] -= vitesse[i];
-    $('#fire'+i).css({
-      'top':positionY[i]+'px'
+     if(positionY[i] <= stop[i]){
+      $('#fire'+i).css({
+        'background-color':'blue',
+        'width':'2px',
+        'height':'2px',
+        'z-index':11
+      });
+      if(duree_explosion[i]==0)
+      {
+        detonation(positionX[i],positionY[i],i)
+      }
+      duree_explosion[i] +=1;
+      if(duree_explosion[i]==22){
+        positionY[i] = randfunc(taille.height,taille.height+100);
+        positionX[i] = randfunc(1,taille.width);
+        duree_explosion[i] =0
+        $('#fire'+i).css({
+          'background-color':'red',
+          'width':'2px',
+          'height':'2px'
+        });
+        for(let j = 1;j<= 5;j++)
+          $('#explose'+i+j).remove();
+      }
+     }
+     else
+      positionY[i] -= vitesse[i];
+      $('#fire'+i).css({
+        'top':positionY[i]+'px',
+        'left':positionX[i]+'px'
     });
    }
   },45);
